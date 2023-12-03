eb70deee1a34847059b578bb6846f2f63f90ac20
Doğancan Turgut <dogancanturgut@Dogancan-Air.bbrouter>
2023-12-03 22:24:25 +0300

    first change

---
+++ ListBuddyV2/ListBuddyV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ListBuddyV2App.swift
+//  ListBuddyV2
+//
+//  Created by Doğancan Turgut on 3.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListBuddyV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
